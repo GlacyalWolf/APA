b97997fb921fb59e032832ee1d1407e796cec0b3
GlacyalWolf <ismaelromero1999@gmail.com>
2019-12-04 22:21:34 +0100

    usuario.java: class added with constructor, set, get

---
+++ app/src/main/java/com/example/apa/Model/usuario.java
@@ -0,0 +1,37 @@
+package com.example.apa.Model;
+
+public class usuario {
+    String username;
+    String password;
+    String mail;
+
+    public usuario(String username, String password, String mail) {
+        this.username = username;
+        this.password = password;
+        this.mail = mail;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+}
