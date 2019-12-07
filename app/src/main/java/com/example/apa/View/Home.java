021ec6b1137ca408d3aeffe736d578cd08b6a18f
GlacyalWolf <ismaelromero1999@gmail.com>
2019-12-07 13:20:44 +0100

    Login.java: added liseners

--- app/src/main/java/com/example/apa/View/Home.java
+++ app/src/main/java/com/example/apa/View/Home.java
@@ -1,5 +1,7 @@
 package com.example.apa.View;
 
+import android.content.Context;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,14 +10,12 @@ import androidx.lifecycle.ViewModelProviders;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.EditText;
 
 import com.example.apa.R;
 import com.example.apa.ViewModel.LoginViewModel;
 
 
-public class Login extends Fragment {
+public class Home extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -25,16 +25,11 @@ public class Login extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    private EditText username, password;
-    private Button register,  login;
-
-    LoginViewModel loginViewModel;
 
 
 
-    public Login() {
-
-
+    public Home() {
+        // Required empty public constructor
     }
 
     /**
@@ -43,11 +38,11 @@ public class Login extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment Login.
+     * @return A new instance of fragment Home.
      */
     // TODO: Rename and change types and number of parameters
-    public static Login newInstance(String param1, String param2) {
-        Login fragment = new Login();
+    public static Home newInstance(String param1, String param2) {
+        Home fragment = new Home();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -68,34 +63,8 @@ public class Login extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        loginViewModel = ViewModelProviders.of(this).get(LoginViewModel.class);
 
-        View ui_layout  = inflater.inflate(R.layout.fragment_login, container, false);
-
-
-
-        username=ui_layout.findViewById(R.id.username_field);
-        password=ui_layout.findViewById(R.id.password_field);
-        register=ui_layout.findViewById(R.id.register_button);
-        login=ui_layout.findViewById(R.id.login_button);
-
-        register.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.container,new RegisterFragment()).commit();
-            }
-        });
-
-        login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.container,new Home()).commit();
-            }
-        });
-
-        return ui_layout;
-
-
+        return inflater.inflate(R.layout.fragment_home, container, false);
     }
 
 
--- app/src/main/java/com/example/apa/ViewModel/HomeViewModel.java
+++ app/src/main/java/com/example/apa/ViewModel/HomeViewModel.java
@@ -3,5 +3,5 @@ package com.example.apa.ViewModel;
 import androidx.lifecycle.ViewModel;
 
 public class HomeViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
+
 }
