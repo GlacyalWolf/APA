2af3ea327b56ff6f2f1f51441d935c1b1a89d0d4
GlacyalWolf <ismaelromero1999@gmail.com>
2019-12-07 11:58:00 +0100

    RegistrarHoras.java: added LoginViewModel.java: added RegistrarHorasViewModel.java: added registrar_horas_ragment.xml: added

---
+++ app/src/main/java/com/example/apa/ViewModel/LoginViewModel.java
@@ -0,0 +1,7 @@
+package com.example.apa.ViewModel;
+
+import androidx.lifecycle.ViewModel;
+
+public class LoginViewModel extends ViewModel {
+
+}
---
+++ app/src/main/java/com/example/apa/ViewModel/RegistrarHorasViewModel.java
@@ -0,0 +1,7 @@
+package com.example.apa.ViewModel;
+
+import androidx.lifecycle.ViewModel;
+
+public class RegistrarHorasViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
